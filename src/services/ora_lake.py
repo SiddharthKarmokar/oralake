04937e6cd07b50da5f1cc12b52cc000daff19312
unknown <siddkarmokar@gmail.com>
2025-10-28 15:21:11 +0530

    add_object now returns object id

--- src/services/ora_lake.py
+++ src/services/ora_lake.py
@@ -1,20 +1,35 @@
 from src import logger
 from src.database import pool
+import oracledb
 
-
-def add_object(name: str, obj_type: str, content: bytes, tags: str, description: str = None, schema_hint: str = None)->bool:
+def add_object(name: str, obj_type: str, content: bytes, tags: str,
+               description: str = None, schema_hint: str = None):
     try:
         with pool.acquire() as conn:
             cursor = conn.cursor()
-            cursor.callproc(
+            object_id = cursor.callfunc(
                 "ora_lake_ops.add_object",
+                oracledb.NUMBER,
                 [name, obj_type, content, tags, description, schema_hint]
             )
             conn.commit()
-        return True
+            logger.info(f"Object added with ID {object_id}")
+            return object_id
     except Exception as e:
-        logger.error(f"Error Occurred at add_object", e)
-        return False
+        logger.error(f"Error Occurred at add_object: {e}")
+        raise
+
+
+def get_object(object_id: int)->bytes:
+    with pool.acquire() as conn:
+        cursor = conn.cursor()
+        result = cursor.callfunc(
+            "ora_lake_ops.get_object",
+            bytes,
+            [object_id]
+        )
+        return result
+
 
 if __name__ == "__main__":
     file_content = b"Hello, this is a test object content."
--- src/sql/ora_lake_init.sql
+++ src/sql/ora_lake_init.sql
@@ -1,19 +1,22 @@
 CREATE OR REPLACE PACKAGE ora_lake_ops AS
-  PROCEDURE add_object(
-    p_name VARCHAR2,
-    p_type VARCHAR2,
-    p_content BLOB,
-    p_tags CLOB,
-    p_description CLOB,
-    p_schema_hint CLOB
-  );
+  FUNCTION add_object(
+    p_name         VARCHAR2,
+    p_type         VARCHAR2,
+    p_content      BLOB,
+    p_tags         CLOB DEFAULT NULL,
+    p_description  CLOB DEFAULT NULL,
+    p_schema_hint  CLOB DEFAULT NULL
+  ) RETURN NUMBER;
+
   FUNCTION get_object(p_id NUMBER) RETURN BLOB;
+
   PROCEDURE tag_object(
-    p_id NUMBER,
-    p_tag VARCHAR2,
-    p_description CLOB DEFAULT NULL,
-    p_schema_hint CLOB DEFAULT NULL
+    p_id           NUMBER,
+    p_tag          VARCHAR2,
+    p_description  CLOB DEFAULT NULL,
+    p_schema_hint  CLOB DEFAULT NULL
   );
+
   FUNCTION query_objects_by_tag(p_tag VARCHAR2) RETURN SYS_REFCURSOR;
 END ora_lake_ops;
 /
