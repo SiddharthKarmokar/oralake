2bad7a33c857965735e6e026440d990e309b2f49
unknown <siddkarmokar@gmail.com>
2025-10-28 15:28:00 +0530

    completed get_objects

--- src/services/ora_lake.py
+++ src/services/ora_lake.py
@@ -19,26 +19,33 @@ def add_object(name: str, obj_type: str, content: bytes, tags: str,
         logger.error(f"Error Occurred at add_object: {e}")
         raise
 
-
 def get_object(object_id: int)->bytes:
-    with pool.acquire() as conn:
-        cursor = conn.cursor()
-        result = cursor.callfunc(
-            "ora_lake_ops.get_object",
-            bytes,
-            [object_id]
-        )
-        return result
+    try:
+        with pool.acquire() as conn:
+            cursor = conn.cursor()
+            result = cursor.callfunc(
+                "ora_lake_ops.get_object",
+                bytes,
+                [object_id]
+            )
+            logger.info(f"Object found: {result}")
+            return result
+    except oracledb.exceptions.DatabaseError as e:
+        error_obj = e.args[0]
+        error_msg = str(error_obj)
 
+        if "ORA-01403" in error_msg:
+            logger.warning(f"Object with ID: {object_id} was not found")
+            return None
+
+        logger.error(f"Database Error occurred at get_object: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error occured at get_object: {e}")
+        raise
 
 if __name__ == "__main__":
-    file_content = b"Hello, this is a test object content."
-
-    add_object(
-        name="sample_doc.txt",
-        obj_type="TEXT",
-        content=file_content,
-        tags="demo,test",
-        description="A simple sample text object",
-        schema_hint=None
+    result = get_object(
+        object_id=9
     )
+    logger.info(result)
